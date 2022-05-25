b7a28285db8a7506db4eeb051b00694f7374fdec
ahmadm24 <ahmadmuji2808@gmail.com>
2022-05-25 22:38:20 +0700

    update routes add login_page routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,6 +1,7 @@
 class Routes {
   static const String HOME_PAGE = '/';
   static const String REGISTER_PAGE = '/register';
+  static const String LOGIN_PAGE = '/login';
   static const String PAJAK_PAGE = '/pajak';
   static const String BERITA_PAGE = '/berita';
   static const String KAS_PAGE = '/kas';

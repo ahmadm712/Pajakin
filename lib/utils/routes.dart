9fce31330494c267ce487844d80d4f5f8e224b5e
ahmadm24 <ahmadmuji2808@gmail.com>
2022-05-25 21:58:57 +0700

    create routes

---
+++ lib/utils/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static const String HOME_PAGE = '/';
+  static const String REGISTER_PAGE = '/register';
+  static const String PAJAK_PAGE = '/pajak';
+  static const String BERITA_PAGE = '/berita';
+  static const String KAS_PAGE = '/kas';
+  static const String PENGATURAN_PAGE = '/pengaturan';
+  static const String PROFILE_PAGE = '/profile';
+}

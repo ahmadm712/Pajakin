1376bad80bf607d3103b618e98a635f08364c4de
ahmadm712 <66357389+ahmadm712@users.noreply.github.com>
2022-06-05 20:48:13 +0700

    Bukukas page (#5)
    * create routes to kaspage
    
    * add new style on styles dart
    
    * create buku kas page but did't completed yet
    
    * add asset to buku kas page
    
    * create routes on buku kas page
    
    * add routes
    
    * create buku kas page but not completed and create pemasukan and pengluaran page
    
    * completed the buku kas page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pajakin/presentation/pages/pages.dart';
+import 'package:pajakin/presentation/pages/pemasukan_page.dart';
+import 'package:pajakin/presentation/pages/pengeluaran_page.dart';
 import 'package:pajakin/utils/routes.dart';
+import 'package:pajakin/utils/styles.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,8 +18,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pajakin',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(primary: kColorPrimary)),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
       onGenerateRoute: (RouteSettings setting) {
@@ -29,6 +32,27 @@ class MyApp extends StatelessWidget {
           case Routes.REGISTER_PAGE:
             return MaterialPageRoute(
               builder: (context) => const RegisterPage(),
+            );
+
+          case Routes.KAS_PAGE:
+            return MaterialPageRoute(
+              builder: (context) => const BukuKasPage(),
+            );
+
+          case Routes.PEMASUKAN_PAGE:
+            final String status = setting.arguments as String;
+            return MaterialPageRoute(
+              builder: (context) => PemasukanPage(
+                status: status,
+              ),
+            );
+
+          case Routes.PENGELUARAN_PAGE:
+            final String status = setting.arguments as String;
+            return MaterialPageRoute(
+              builder: (context) => PengeluaranPage(
+                status: status,
+              ),
             );
 
           default:
--- lib/presentation/pages/pages.dart
+++ lib/presentation/pages/pages.dart
@@ -1,2 +1,3 @@
 export 'home_page.dart';
 export 'register.dart';
+export 'bukukas_page.dart';

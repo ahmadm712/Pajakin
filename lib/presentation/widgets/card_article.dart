e4880e6237d6a0a080d8c35dbc9dda7b61e561b8
19193-IbrohimHusain <84296114+19193-IbrohimHusain@users.noreply.github.com>
2022-06-05 17:24:15 +0700

    Update card_article.dart

---
+++ lib/presentation/widgets/card_article.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:pajakin/data/models/article.dart';
+import 'package:pajakin/utils/routes.dart';
+
+import '../../utils/global_function.dart';
+
+class CardArticle extends StatelessWidget {
+  final Article article;
+  const CardArticle({Key? key, required this.article}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, Routes.DETAIL_BERITA_PAGE);
+        },
+        child: ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 35.0, vertical: 8.0),
+          leading: Hero(
+            tag: article.urlToImage!,
+            child: Image.network(
+              article.urlToImage!,
+              width: 100,
+            ),
+          ),
+          title: Text(
+            article.title,
+            style: GlobalFunctions.textTheme(context: context)
+                .headline6!
+                .copyWith(color: Colors.white, fontSize: 16),
+          ),
+          subtitle: Text(article.author ?? "",
+              style: GlobalFunctions.textTheme(context: context)
+                  .subtitle1!
+                  .copyWith(color: Colors.white, fontSize: 14)),
+        ));
+  }
+}

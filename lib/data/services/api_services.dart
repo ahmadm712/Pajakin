6e2dc2c47206531648ee77699d69c06e15659ebd
19193-IbrohimHusain <84296114+19193-IbrohimHusain@users.noreply.github.com>
2022-06-05 17:19:55 +0700

    Update api_services.dart

--- lib/data/services/api_services.dart
+++ lib/data/services/api_services.dart
@@ -3,14 +3,13 @@ import 'package:http/http.dart' as http;
 import 'package:pajakin/data/models/article.dart';
 
 class ApiService {
-  static final String _baseUrl = 'https://newsapi.org/v2/';
-  static final String _apiKey = '29e44efa62434079a27fcc7e30717fd4';
-  static final String _category = 'business';
-  static final String _country = 'id';
+  static const String _baseUrl = 'https://newsapi.org/v2/';
+  static const String _apiKey = '29e44efa62434079a27fcc7e30717fd4';
+  static const String _category = 'business';
+  static const String _country = 'id';
 
   Future<ArticlesResult> topHeadlines() async {
-    final response = await http.get(Uri.parse(_baseUrl +
-        "top-headlines?country=$_country&category=$_category&apiKey=$_apiKey"));
+    final response = await http.get(Uri.parse("${_baseUrl}top-headlines?country=$_country&category=$_category&apiKey=$_apiKey"));
     if (response.statusCode == 200) {
       return ArticlesResult.fromJson(json.decode(response.body));
     } else {

6603743ebc90c0ec14f7e09a8eef132451a93e70
19193-IbrohimHusain <84296114+19193-IbrohimHusain@users.noreply.github.com>
2022-06-05 17:30:33 +0700

    Update routes.dart
    Add new routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -5,6 +5,7 @@ class Routes {
   static const String PAJAK_PAGE = '/pajak';
   static const String BERITA_PAGE = '/berita';
   static const String DETAIL_BERITA_PAGE = '/detail';
+  static const String WEB_VIEW_PAGE = '/webview';
   static const String KAS_PAGE = '/kas';
   static const String PENGATURAN_PAGE = '/pengaturan';
   static const String PROFILE_PAGE = '/profile';

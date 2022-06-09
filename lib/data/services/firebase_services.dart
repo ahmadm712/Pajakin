8efbddfdf92511d113a492717fefe7659850b446
fanolans <mailenolan@gmail.com>
2022-06-09 20:11:33 +0700

    update firebase services

--- lib/data/services/firebase_services.dart
+++ lib/data/services/firebase_services.dart
@@ -59,6 +59,202 @@ class FirebaseServices {
     }
   }
 
+  static Future<void> updateInformationAccount({
+    required String name,
+    required String umkmName,
+    required String email,
+    required String password,
+  }) async {
+    DocumentReference documentReferencer = _mainCollection.doc(userUid);
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "userName": name,
+      "umkmName": umkmName,
+      "email": email,
+      "password": password,
+    };
+
+    await documentReferencer
+        .update(data)
+        .whenComplete(() => print("Information item updated in the database"))
+        .catchError((e) => print(e));
+  }
+
+/*
+import 'package:apple_sign_in/apple_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Stream<String> get onAuthStateChanged => _firebaseAuth.onAuthStateChanged.map(
+        (FirebaseUser user) => user?.uid,
+      );
+
+  // GET UID
+  Future<String> getCurrentUID() async {
+    return (await _firebaseAuth.currentUser()).uid;
+  }
+
+  // GET CURRENT USER
+  Future getCurrentUser() async {
+    return await _firebaseAuth.currentUser();
+  }
+
+  // Email & Password Sign Up
+  Future<String> createUserWithEmailAndPassword(
+      String email, String password, String name) async {
+    final authResult = await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    // Update the username
+    await updateUserName(name, authResult.user);
+    return authResult.user.uid;
+  }
+
+  Future updateUserName(String name, FirebaseUser currentUser) async {
+    var userUpdateInfo = UserUpdateInfo();
+    userUpdateInfo.displayName = name;
+    await currentUser.updateProfile(userUpdateInfo);
+    await currentUser.reload();
+  }
+
+  // Email & Password Sign In
+  Future<String> signInWithEmailAndPassword(
+      String email, String password) async {
+    return (await _firebaseAuth.signInWithEmailAndPassword(
+            email: email, password: password))
+        .user
+        .uid;
+  }
+
+  // Sign Out
+  signOut() {
+    return _firebaseAuth.signOut();
+  }
+
+  // Reset Password
+  Future sendPasswordResetEmail(String email) async {
+    return _firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+
+  // Create Anonymous User
+  Future singInAnonymously() {
+    return _firebaseAuth.signInAnonymously();
+  }
+
+  Future convertUserWithEmail(
+      String email, String password, String name) async {
+    final currentUser = await _firebaseAuth.currentUser();
+
+    final credential =
+        EmailAuthProvider.getCredential(email: email, password: password);
+    await currentUser.linkWithCredential(credential);
+    await updateUserName(name, currentUser);
+  }
+
+  Future convertWithGoogle() async {
+    final currentUser = await _firebaseAuth.currentUser();
+    final GoogleSignInAccount account = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication _googleAuth = await account.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      idToken: _googleAuth.idToken,
+      accessToken: _googleAuth.accessToken,
+    );
+    await currentUser.linkWithCredential(credential);
+    await updateUserName(_googleSignIn.currentUser.displayName, currentUser);
+  }
+
+  // GOOGLE
+  Future<String> signInWithGoogle() async {
+    final GoogleSignInAccount account = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication _googleAuth = await account.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      idToken: _googleAuth.idToken,
+      accessToken: _googleAuth.accessToken,
+    );
+    return (await _firebaseAuth.signInWithCredential(credential)).user.uid;
+  }
+
+  // APPLE
+  Future signInWithApple() async {
+    final AuthorizationResult result = await AppleSignIn.performRequests([
+      AppleIdRequest(requestedScopes: [Scope.email, Scope.fullName])
+    ]);
+
+    switch (result.status) {
+      case AuthorizationStatus.authorized:
+        final AppleIdCredential _auth = result.credential;
+        final OAuthProvider oAuthProvider =
+            new OAuthProvider(providerId: "apple.com");
+
+        final AuthCredential credential = oAuthProvider.getCredential(
+          idToken: String.fromCharCodes(_auth.identityToken),
+          accessToken: String.fromCharCodes(_auth.authorizationCode),
+        );
+
+        await _firebaseAuth.signInWithCredential(credential);
+
+        // update the user information
+        if (_auth.fullName != null) {
+          _firebaseAuth.currentUser().then((value) async {
+            UserUpdateInfo user = UserUpdateInfo();
+            user.displayName =
+                "${_auth.fullName.givenName} ${_auth.fullName.familyName}";
+            await value.updateProfile(user);
+          });
+        }
+
+        break;
+
+      case AuthorizationStatus.error:
+        print("Sign In Failed ${result.error.localizedDescription}");
+        break;
+
+      case AuthorizationStatus.cancelled:
+        print("User Cancled");
+        break;
+    }
+  }
+}
+
+class NameValidator {
+  static String validate(String value) {
+    if (value.isEmpty) {
+      return "Name can't be empty";
+    }
+    if (value.length < 2) {
+      return "Name must be at least 2 characters long";
+    }
+    if (value.length > 50) {
+      return "Name must be less than 50 characters long";
+    }
+    return null;
+  }
+}
+
+class EmailValidator {
+  static String validate(String value) {
+    if (value.isEmpty) {
+      return "Email can't be empty";
+    }
+    return null;
+  }
+}
+
+class PasswordValidator {
+  static String validate(String value) {
+    if (value.isEmpty) {
+      return "Password can't be empty";
+    }
+    return null;
+  }
+}*/
+
   // static Future<void> updateItem({
   //   required DateTime time,
   //   required String name,

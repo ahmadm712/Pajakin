b6be9963af86d72b95b1885f0d842ac8a0a98d45
fanolans <mailenolan@gmail.com>
2022-06-09 20:10:27 +0700

    add user model

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,1 +1,50 @@
+import 'dart:convert';
 
+class UserUmkm {
+  String id;
+  String username;
+  String email;
+  String umkmname;
+  String password;
+  int saldo;
+  UserUmkm({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.umkmname,
+    required this.password,
+    required this.saldo,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'username': username,
+      'email': email,
+      'umkmname': umkmname,
+      'password': password,
+      'saldo': saldo,
+    };
+  }
+
+  factory UserUmkm.fromMap(Map<String, dynamic> map) {
+    return UserUmkm(
+      id: map['id']?.toInt() ?? 0,
+      username: map['username'] ?? '',
+      email: map['email'] ?? '',
+      umkmname: map['umkmname'] ?? '',
+      password: map['password'] ?? '',
+      saldo: map['saldo']?.toInt() ?? 0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserUmkm.fromJson(String source) =>
+      UserUmkm.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'User(id: $id, username: $username, email: $email, umkmname: $umkmname, password: $password, saldo: $saldo)';
+  }
+}

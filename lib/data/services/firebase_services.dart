13ca6c2d0fbb40e6cfc305ffa6c0d93c9d16dbc3
fanolans <mailenolan@gmail.com>
2022-06-09 15:20:39 +0700

    update firebase services

--- lib/data/services/firebase_services.dart
+++ lib/data/services/firebase_services.dart
@@ -59,6 +59,29 @@ class FirebaseServices {
     }
   }
 
+  static Future<User?> signInUsingEmailPassword({
+    required String email,
+    required String password,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided.');
+      }
+    }
+
+    return user;
+  }
   // static Future<void> updateItem({
   //   required DateTime time,
   //   required String name,

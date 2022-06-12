d958b404e383bfdd89b4122e4bbee927571c8d48
ahmadm712 <ahmadmuji2808@gmail.com>
2022-06-12 17:37:57 +0700

    implement function on page each other but to show list of pemasukan dan pengeluaran did't

--- lib/presentation/pages/pengeluaran_page.dart
+++ lib/presentation/pages/pengeluaran_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pajakin/data/services/firebase_services.dart';
 import 'package:pajakin/utils/constans.dart';
 
 import 'package:pajakin/utils/global_function.dart';
@@ -27,6 +28,12 @@ class _PengeluaranPageState extends State<PengeluaranPage> {
     super.dispose();
     keteranganController.dispose();
     pengeluaranController.dispose();
+  }
+
+  void clearField() {
+    date = "";
+    keteranganController.clear();
+    pengeluaranController.clear();
   }
 
   @override
@@ -228,7 +235,7 @@ class _PengeluaranPageState extends State<PengeluaranPage> {
                       margin: const EdgeInsets.only(top: 11),
                       child: TextFormField(
                         controller: pengeluaranController,
-                        keyboardType: TextInputType.emailAddress,
+                        keyboardType: TextInputType.number,
                         validator: (value) {
                           if (value!.isEmpty) {
                             return 'Tolong isi jumlah Pengeluaran';
@@ -286,7 +293,29 @@ class _PengeluaranPageState extends State<PengeluaranPage> {
                       width: 154,
                       height: 45,
                       child: ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          if (GlobalFunctions.validate(
+                              context: context, formkey: formKey)) {
+                            if (widget.status == 'tambah') {
+                              FirebaseServices.addPengeluaran(
+                                  date: date,
+                                  description: keteranganController.text,
+                                  jumlahPengeluaran: int.parse(
+                                    pengeluaranController.text,
+                                  )).then((value) {
+                                GlobalFunctions.scaffoldMessage(
+                                    context: context,
+                                    message: 'Pengeluaran Succes Ditambahkan',
+                                    color: Colors.green);
+                              }).catchError((e) {
+                                GlobalFunctions.scaffoldMessage(
+                                    context: context,
+                                    message: e,
+                                    color: Colors.green);
+                              });
+                            }
+                          }
+                        },
                         style: ElevatedButton.styleFrom(
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(10))),

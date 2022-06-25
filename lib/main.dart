2fcbb6cbaca1e1fbc09b9745cb7be48d52dc891d
ahmadm712 <ahmadmuji2808@gmail.com>
2022-06-25 22:43:08 +0700

    create auto sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pajakin/data/services/api_services.dart';
+import 'package:pajakin/data/services/firebase_services.dart';
 import 'package:pajakin/presentation/pages/article_list_page.dart';
 import 'package:pajakin/presentation/pages/article_web_view.dart';
 import 'package:pajakin/presentation/pages/login_page.dart';
@@ -46,94 +48,111 @@ class MyApp extends StatelessWidget {
           ChangeNotifierProvider<NewsProvider>(
             create: (_) => NewsProvider(apiService: ApiService()),
           ),
-          ChangeNotifierProvider<SchedulingProvider>(create: (_) => SchedulingProvider())
+          ChangeNotifierProvider<SchedulingProvider>(
+              create: (_) => SchedulingProvider())
         ],
-        child: MaterialApp(
-          title: 'Pajakin',
-          theme: ThemeData(
-            colorScheme:
-                ColorScheme.fromSwatch().copyWith(primary: kColorPrimary),
-          ),
-          debugShowCheckedModeBanner: false,
-          home: const SplashScreen(),
-          onGenerateRoute: (RouteSettings setting) {
-            switch (setting.name) {
-              case Routes.HOME_PAGE:
-                return MaterialPageRoute(
-                  builder: (context) => const HomePage(),
-                );
-
-              case Routes.REGISTER_PAGE:
-                return MaterialPageRoute(
-                  builder: (context) => const RegisterPage(),
-                );
-
-              case Routes.LOGIN_PAGE:
-                return MaterialPageRoute(
-                  builder: (context) => const LoginPage(),
-                );
-
-              case Routes.BERITA_PAGE:
-                return MaterialPageRoute(
-                  builder: (context) => const ArticleListPage(),
-                );
-
-              case Routes.WEB_VIEW_PAGE:
-                final url = setting.arguments as String;
-                return MaterialPageRoute(
-                  builder: (context) => ArticleWebView(url: url),
-                );
-
-              case Routes.PAJAK_PAGE:
-                return MaterialPageRoute(
-                  builder: (context) => const PajakPage(),
-                );
-
-              case Routes.SETTINGS_PAGE:
-                return MaterialPageRoute(
-                  builder: (context) => const SettingsPage(),
-                );
-
-              case Routes.INFORMATION_ACCOUNT_PAGE:
-                return MaterialPageRoute(
-                  builder: (context) => const InformationAccountPage(),
-                );
-
-              case Routes.CHANGE_PASSWORD_PAGE:
-                return MaterialPageRoute(
-                  builder: (context) => const ChangePasswordPage(),
-                );
-              case Routes.KAS_PAGE:
-                return MaterialPageRoute(
-                  builder: (context) => const BukuKasPage(),
-                );
-
-              case Routes.PEMASUKAN_PAGE:
-                final data = setting.arguments as Map<String, dynamic>;
-                return MaterialPageRoute(
-                  builder: (context) => PemasukanPage(
-                    data: data,
-                  ),
-                );
-
-              case Routes.PENGELUARAN_PAGE:
-                final data = setting.arguments as Map<String, dynamic>;
-                return MaterialPageRoute(
-                  builder: (context) => PengeluaranPage(
-                    data: data,
-                  ),
-                );
-
-              default:
-                return MaterialPageRoute(
-                  builder: (context) => const Scaffold(
+        child: StreamBuilder<User?>(
+            stream: auth.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const MaterialApp(
+                  home: Scaffold(
                     body: Center(
-                      child: Text('Page Not Found :('),
+                      child: CircularProgressIndicator(),
                     ),
                   ),
                 );
-            }
-          },
-        ));
+              }
+
+              return MaterialApp(
+                title: 'Pajakin',
+                theme: ThemeData(
+                  colorScheme:
+                      ColorScheme.fromSwatch().copyWith(primary: kColorPrimary),
+                ),
+                debugShowCheckedModeBanner: false,
+                home: snapshot.data == null
+                    ? const SplashScreen()
+                    : const HomePage(),
+                onGenerateRoute: (RouteSettings setting) {
+                  switch (setting.name) {
+                    case Routes.HOME_PAGE:
+                      return MaterialPageRoute(
+                        builder: (context) => const HomePage(),
+                      );
+
+                    case Routes.REGISTER_PAGE:
+                      return MaterialPageRoute(
+                        builder: (context) => const RegisterPage(),
+                      );
+
+                    case Routes.LOGIN_PAGE:
+                      return MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      );
+
+                    case Routes.BERITA_PAGE:
+                      return MaterialPageRoute(
+                        builder: (context) => const ArticleListPage(),
+                      );
+
+                    case Routes.WEB_VIEW_PAGE:
+                      final url = setting.arguments as String;
+                      return MaterialPageRoute(
+                        builder: (context) => ArticleWebView(url: url),
+                      );
+
+                    case Routes.PAJAK_PAGE:
+                      return MaterialPageRoute(
+                        builder: (context) => const PajakPage(),
+                      );
+
+                    case Routes.SETTINGS_PAGE:
+                      return MaterialPageRoute(
+                        builder: (context) => const SettingsPage(),
+                      );
+
+                    case Routes.INFORMATION_ACCOUNT_PAGE:
+                      return MaterialPageRoute(
+                        builder: (context) => const InformationAccountPage(),
+                      );
+
+                    case Routes.CHANGE_PASSWORD_PAGE:
+                      return MaterialPageRoute(
+                        builder: (context) => const ChangePasswordPage(),
+                      );
+                    case Routes.KAS_PAGE:
+                      return MaterialPageRoute(
+                        builder: (context) => const BukuKasPage(),
+                      );
+
+                    case Routes.PEMASUKAN_PAGE:
+                      final data = setting.arguments as Map<String, dynamic>;
+                      return MaterialPageRoute(
+                        builder: (context) => PemasukanPage(
+                          data: data,
+                        ),
+                      );
+
+                    case Routes.PENGELUARAN_PAGE:
+                      final data = setting.arguments as Map<String, dynamic>;
+                      return MaterialPageRoute(
+                        builder: (context) => PengeluaranPage(
+                          data: data,
+                        ),
+                      );
+
+                    default:
+                      return MaterialPageRoute(
+                        builder: (context) => const Scaffold(
+                          body: Center(
+                            child: Text('Page Not Found :('),
+                          ),
+                        ),
+                      );
+                  }
+                },
+              );
+            }));
   }
 }

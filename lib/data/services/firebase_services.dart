4569d16328724e65f912dc3ff855ceadf6f62544
ahmadm712 <ahmadmuji2808@gmail.com>
2022-06-25 22:43:30 +0700

    create sign in with google and register auto with google sign in

--- lib/data/services/firebase_services.dart
+++ lib/data/services/firebase_services.dart
@@ -1,23 +1,65 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:pajakin/data/models/kas_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pajakin/data/models/pemasukan_model.dart';
 import 'package:pajakin/data/models/pengeluaran_model.dart';
 import 'package:pajakin/data/models/user_model.dart';
+import 'package:pajakin/utils/routes.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 FirebaseAuth auth = FirebaseAuth.instance;
 final CollectionReference _mainCollection = _firestore.collection('user');
-final CollectionReference _secondCollection = _firestore.collection('kas');
 final CollectionReference _pemasukanCollection =
     _firestore.collection('pemasukan');
 final CollectionReference _pengeluaranCollection =
     _firestore.collection('pengeluaran');
+GoogleSignIn googleSignIn = GoogleSignIn();
 
 class FirebaseServices {
   static String? userUid;
+
+  Future<UserCredential> signInWithGoogleNew() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  Future<bool> searchUser({required String id}) async {
+    final DocumentSnapshot doc = await _mainCollection.doc(id).get();
+
+    return doc.exists ? true : false;
+  }
+
+  Future signOut({required BuildContext context}) async {
+    await auth.signOut().whenComplete(() async {
+      if (await googleSignIn.isSignedIn()) {
+        await googleSignIn
+            .signOut()
+            .whenComplete(() => Navigator.of(context).pushNamedAndRemoveUntil(
+                  Routes.LOGIN_PAGE,
+                  (route) => false,
+                ));
+      } else {
+        Navigator.of(context).pushNamedAndRemoveUntil(
+          Routes.LOGIN_PAGE,
+          (route) => false,
+        );
+      }
+    });
+  }
 
   static Future<void> register({
     required String name,
@@ -60,6 +102,36 @@ class FirebaseServices {
     }
   }
 
+  Future<void> registergoogleSignIn({required User user}) async {
+    try {
+      final userUmkm = UserUmkm(
+        id: user.uid,
+        username: user.displayName!,
+        email: user.email!,
+        umkmname: '',
+        password: user.uid,
+      );
+
+      return _mainCollection
+          .doc(auth.currentUser!.uid)
+          .set(userUmkm.toMap())
+          .whenComplete(() => print("User Has been Created"))
+          .catchError(
+            (e) => print(
+              e,
+            ),
+          );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   static Future<User?> signInUsingEmailPassword({
     required String email,
     required String password,
@@ -81,12 +153,6 @@ class FirebaseServices {
       }
     }
     return user;
-  }
-
-  static Future<void> signOut() async {
-    await FirebaseAuth.instance.signOut().then(((value) {
-      print("User Has been Signed Out");
-    }));
   }
 
   static Future<void> updateInformationAccount({
